74040b75e45f3ed4d00f91c0be2fe79f239c6de4
[Emilola Divine] <[emiloladivine123@gmail.com]>
2023-12-07 21:50:50 +0100

    formatting code

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 const questions = document.querySelectorAll(".question");
 
-
 questions.forEach((question) => {
    question.addEventListener("click", () => {
-let answer = question.nextElementSibling;
-answer.classList.toggle("close");
+      let answer = question.nextElementSibling;
+      answer.classList.toggle("close");
 
-//change icon
-     let plusIcon = question.querySelectorAll(".plus");
-     let minusIcon = question.querySelectorAll(".minus");
+      //change icon
+      let plusIcon = question.querySelectorAll(".plus");
+      let minusIcon = question.querySelectorAll(".minus");
 
-     plusIcon.forEach((icon) => {
-        if (answer.classList.contains("close")) {
-           icon.style.display = "block"; // Show plus icon
-        } else {
-           icon.style.display = "none"; // Hide plus icon
-        }
-     });
+      plusIcon.forEach((icon) => {
+         if (answer.classList.contains("close")) {
+            icon.style.display = "block"; // Show plus icon
+         } else {
+            icon.style.display = "none"; // Hide plus icon
+         }
+      });
 
-     minusIcon.forEach((icon) => {
-        if (answer.classList.contains("close")) {
-           icon.style.display = "none"; // Hide minus icon
-        } else {
-           icon.style.display = "block"; // Show minus icon
-        }
-     });
-   })
-})
+      minusIcon.forEach((icon) => {
+         if (answer.classList.contains("close")) {
+            icon.style.display = "none"; // Hide minus icon
+         } else {
+            icon.style.display = "block"; // Show minus icon
+         }
+      });
+   });
+});
